7eeddd2864e00d8bd0f72326fbc264f08285cf5f
andre agel <andreagel9412@gmail.com>
2020-06-14 17:41:34 -0300

    Next-level-Week

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import routes from './routes';
+import path from 'path';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(cors());
+
+app.listen(3333);
